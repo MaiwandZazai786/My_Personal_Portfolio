aa41cb4fa1505890dafaa4b85a277617d4632f0f
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2025-08-15 22:12:16 +0200

    foother text changed and year added to the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,39 +1,55 @@
-import React from 'react'
-import './footer.css'
-import logo from '../../assets/logo.png';
-import { BsFacebook } from 'react-icons/bs';
-import { BsInstagram } from 'react-icons/bs';
-import { ImTwitter } from 'react-icons/im';
+import React from "react";
+import "./footer.css";
+import logo from "../../assets/logo.png";
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { ImTwitter } from "react-icons/im";
 
 const Footer = () => {
   return (
-    <section id='footer'>
+    <section id="footer">
       <div className="logo">
-        <img src={logo} alt="" className='footer__logo' />
+        <img src={logo} alt="" className="footer__logo" />
       </div>
 
-      <ul className='permalinks'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#expeirence">Expeirence</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#expeirence">Expeirence</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com" target='_black'><BsFacebook/></a>
-        <a href="http://instagram.com" target='_black'><BsInstagram/></a>
-        <a href="https://twitter.com" target='_black'><ImTwitter/></a>
+        <a href="https://facebook.com" target="_black">
+          <BsFacebook />
+        </a>
+        <a href="http://instagram.com" target="_black">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com" target="_black">
+          <ImTwitter />
+        </a>
       </div>
-
 
       <div className="footer_copyright">
         <small>
-            &copy; Being a Programmer. All rights reserved.
+          &copy; {new Date().getFullYear()} Maiwand Zazai — Full-Stack Developer
+          Portfolio. All rights reserved.
         </small>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
