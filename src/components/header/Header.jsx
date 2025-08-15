4821446b2d929c52023479bef6ad6ae09720fb0b
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2025-08-15 23:00:53 +0200

    text fixings

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,39 +6,13 @@ import HeaderSocials from "../HeaderSocials";
 
 const Header = () => {
   const text = "Maiwand Zazai";
-  const [displayText, setDisplayText] = useState("");
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const speed = isDeleting ? 100 : 150;
-    const timeout = setTimeout(() => {
-      if (!isDeleting) {
-        setDisplayText(text.substring(0, index + 1));
-        setIndex(index + 1);
-
-        if (index + 1 === text.length) {
-          setTimeout(() => setIsDeleting(true), 1000);
-        }
-      } else {
-        setDisplayText(text.substring(0, index - 1));
-        setIndex(index - 1);
-
-        if (index - 1 === 0) {
-          setIsDeleting(false);
-        }
-      }
-    }, speed);
-
-    return () => clearTimeout(timeout);
-  }, [index, isDeleting, text]);
 
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1 className="typing-cursor">
-          {displayText}
+          {text}
           <span className="cursor"></span>
         </h1>
 
