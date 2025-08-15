28afef32854950c53e95e08b6f43d4886824a32b
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2025-08-15 22:00:57 +0200

    changes to the images on the page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 import CTA from "../CTA";
-import ME from "../../assets/iam-removebg-preview.png";
+import ME from "../../assets/me2-removebg-preview.png";
 import HeaderSocials from "../HeaderSocials";
+
 const Header = () => {
+  const text = "Maiwand Zazai";
+  const [displayText, setDisplayText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const speed = isDeleting ? 100 : 150;
+    const timeout = setTimeout(() => {
+      if (!isDeleting) {
+        setDisplayText(text.substring(0, index + 1));
+        setIndex(index + 1);
+
+        if (index + 1 === text.length) {
+          setTimeout(() => setIsDeleting(true), 1000);
+        }
+      } else {
+        setDisplayText(text.substring(0, index - 1));
+        setIndex(index - 1);
+
+        if (index - 1 === 0) {
+          setIsDeleting(false);
+        }
+      }
+    }, speed);
+
+    return () => clearTimeout(timeout);
+  }, [index, isDeleting, text]);
+
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm </h5>
-        <h1>Maiwand Zazai</h1>
+        <h5>Hello I'm</h5>
+        <h1 className="typing-cursor">
+          {displayText}
+          <span className="cursor"></span>
+        </h1>
 
         <h5 className="text-light">Full-Stack Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
-          <img src={ME} alt="ME" />
+          <img src={ME} alt="ME" style={{ width: "400px", height: "450px" }} />
         </div>
+
         <a href="#contact" className="scroll__down">
-          Scroll Dwon
+          Scroll Down
         </a>
       </div>
     </header>
